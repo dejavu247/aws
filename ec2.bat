5c7e765259633a7cd36162b21ae8703f77958e46
dejavu247 <69946279+dejavu247@users.noreply.github.com>
2020-08-28 21:10:04 -0500

    Update ec2.bat

--- ec2.bat
+++ ec2.bat
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-0239d3998515e9ed1 --count 1 --instance-type t2.micro --key-name test --security-groups test
+aws ec2 run-instances --image-id ami-0239d3998515e9ed1 --count 1 --instance-type t2.micro --key-name test --security-groups test --region us-east-2
